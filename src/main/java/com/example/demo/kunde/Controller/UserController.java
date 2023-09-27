5ac8dde61722be9aacf28c3cdda68e53810bbba1
Mouhammad Bdeir <84782106+MouhammadBdeir@users.noreply.github.com>
2023-09-27 12:50:48 +0200

    update admin Portal
    add feedback portal+ add customer portal +add button to delete  feedback and customer + fix bugs by cancel deleting poject

--- src/main/java/com/example/demo/kunde/Controller/UserController.java
+++ src/main/java/com/example/demo/kunde/Controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -33,5 +35,28 @@ public class UserController{
         model.addAttribute("users", users);
         return "AdminPortal";
     }
-
+    @GetMapping("/admin/feedbackportal")
+    public String showFeedbackPortal(Model model) {
+        System.out.println("showFeedbackPortal sucess");
+        List<Feedback> Feedbacks = feedbackService.getAllFeedbacks();
+        model.addAttribute("feedbacks", Feedbacks);
+        return "AdminFeedbackPortal";
+    }
+    @GetMapping("/admin/customerportal")
+    public String showCustomerPortal(Model model) {
+        System.out.println("showshowCustomerPortal sucess");
+        List<Customer> customers = customerService.getAllCustomers();
+        model.addAttribute("customers", customers);
+        return "CustomerPortal";
+    }
+    @PostMapping("/admin/delete/feedback/{id}")
+    public String deleteFeedback(@PathVariable Long id) {
+        feedbackService.deleteFeedback(id);
+        return "redirect:/admin";
+    }
+    @PostMapping("/admin/delete/customer/{id}")
+    public String deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return "redirect:/admin";
+    }
 }
--- src/main/resources/static/js/project-script.js
+++ src/main/resources/static/js/project-script.js
@@ -18,3 +18,52 @@ document.querySelector('.last').addEventListener('click', () => {
     currentSlide = slides.length - 1;
     showSlide(currentSlide);
 });
+
+    function deleteProject(event) {
+    event.preventDefault(); // Prevent the form submission
+
+    if (confirm('Are you sure you want to delete this project?')) {
+    var form = document.getElementById('deleteForm');
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', form.getAttribute('action'), true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xhr.onload = function() {
+    if (xhr.status === 200) {
+    // Handle success, e.g., update the UI
+    alert('deleted successfully');
+    // Optionally, you can redirect or update the page here
+} else {
+    // Handle errors, e.g., display an error message
+    alert('An error occurred while deleting ');
+}
+};
+
+    xhr.send(''); // Send the POST request
+}
+}
+function deletePost(event ) {
+    event.preventDefault(); // Prevent the form submission
+
+    if (confirm('Are you sure you want to delete this project?')) {
+        var form = document.getElementById('deleteFormFeedback');
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', form.getAttribute('action'), true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                // Handle success, e.g., update the UI
+                alert('deleted successfully');
+                // Optionally, you can redirect or update the page here
+            } else {
+                // Handle errors, e.g., display an error message
+                alert('An error occurred while deleting ');
+            }
+        };
+
+        xhr.send(''); // Send the POST request
+    }
+}
